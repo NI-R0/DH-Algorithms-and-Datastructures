4fa6336c2231387a27d1bd52c19d9efa9532e2e8
Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>
2022-07-10 18:55:05 +0200

    Added files and implementation for data structure: Menge (Array)
    Signed-off-by: Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>

--- Main.c
+++ Main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "List.h"
-#include "Stack_List.h"
-#include "Stack_Array.h"
+#include "Menge_Array.h"
 
 int main(int argc, char* argv[]) {
 	
+	Menge m = create(2);
+
+	Menge n = create(5);
+
+	m = add(m, 4);
+	m = add(m, 5);
+
+	printMenge(m);
+
+	n = add(n, 1);
+	n = add(n, 2);
+	n = add(n, 4);
+	n = add(n, 3);
+	n = add(n, 5);
+
+	printMenge(n);
+
+	Menge z = average(m, n);
+
+	printMenge(z);
 	
 	return 0;
 }
